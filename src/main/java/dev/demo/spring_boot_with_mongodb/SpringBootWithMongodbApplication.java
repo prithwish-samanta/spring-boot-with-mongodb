6da7b3410ff5e0f1e370dde24fa0b1ee43a00234
prithwish <wprith@gmail.com>
2025-04-25 21:21:11 +0530

    initial project setup

---
+++ src/main/java/dev/demo/spring_boot_with_mongodb/SpringBootWithMongodbApplication.java
@@ -0,0 +1,13 @@
+package dev.demo.spring_boot_with_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/demo/spring_boot_with_mongodb/SpringBootWithMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.demo.spring_boot_with_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
